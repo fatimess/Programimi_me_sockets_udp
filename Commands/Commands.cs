7a7fa0e7cf94720974396d0c6ffa2ba6392697ce
JonaElezi <jona.elezi@student.uni-pr.edu>
2026-04-19 15:25:12 +0200

    Update Commands.cs

--- Commands/Commands.cs
+++ Commands/Commands.cs
@@ -5,7 +5,7 @@ using System.Linq;
 public class Commands
 {
 	private static readonly string folder = "files";
-	public static string Execute (string command)
+	public static string Execute(string command)
 	{
 		if (!Directory.Exists(folder))
 			Directory.CreateDirectory(folder);
@@ -22,30 +22,82 @@ public class Commands
 				return ReadFile(parts[1]);
 
 			case "/delete":
-				if (parts.Length<2) return "Jep emrin e file";
-                return DeleteFile(parts[1])
+				if (parts.Length < 2) return "Jep emrin e file";
+				return DeleteFile(parts[1])
 
 
-                    case "/search":
-                if (parts.Length < 2) return "Jep emrin e file";
-                return SearchFile(parts[1])
+
+					case "/search":
+				if (parts.Length < 2) return "Jep emrin e file";
+				return SearchFile(parts[1])
 
 
-                    case "/info":
-                if (parts.Length < 2) return "Jep emrin e file";
-                return InfoFile(parts[1])
+
+					case "/info":
+				if (parts.Length < 2) return "Jep emrin e file";
+				return InfoFile(parts[1])
 
 
 					case "/upload":
 				return "Upload do te trajtohet nga klienti";
 
-            case "/download":
-                return "Download do te trajtohet nga klienti";
+			case "/download":
+				return "Download do te trajtohet nga klienti";
 
 			default:
 				return "Komanda eshte e panjohur!";
 
 
-        }
+		}
 	}
+
+	static string ListFiles()
+	{
+		var files = Directory.GetFiles(files)
+			.Select(Path.GetFileName);
+
+		return files.Any() ? string.Join("\n", files) : "Nuk ka file";
+	}
+
+	static string ReadFile(string name)
+	{
+		string path = path.Combine(folder, name);
+
+		if (!Files.Exists(path))
+			return "File nuk ekziston!";
+
+		return Files.ReadAllText(path);
+	}
+	static string DeleteFile(string name)
+	{ 
+		string path = Path.Combine(folder, name);
+
+        if (!File.Exists(path))
+            return "File nuk ekziston!";
+
+        File.Delete(path);
+        return "File u fshi!";
+    }
+
+    static string Search(string keyword)
+    {
+        var files = Directory.GetFiles(folder)
+            .Where(f => File.ReadAllText(f)
+            .Contains(keyword, StringComparison.OrdinalIgnoreCase))
+            .Select(Path.GetFileName);
+
+        return files.Any() ? string.Join("\n", files) : "Nuk u gjet asgje.";
+    }
+
+    static string Info(string name)
+    {
+        string path = Path.Combine(folder, name);
+
+        if (!File.Exists(path))
+            return "File nuk ekziston!";
+
+        FileInfo f = new FileInfo(path);
+
+        return $"Emri: {f.Name}\nMadhesia: {f.Length} bytes\nKrijuar: {f.CreationTime}";
+    }
 }
